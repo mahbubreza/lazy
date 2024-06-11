193cf4158a3353b96459b2cfc05f526b7e873a07
mahbubreza <mahbubrezachoudhury@gmail.com>
2024-06-11 17:03:22 +0600

    from office

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import ColorDemo from "./components/ColorDemo";
-import ShapeDemo from "./components/ShapeDemo";
-import SizeDemo from "./components/SizeDemo";
+import demos from "./data/demos";
+
 
 export default function App() {
   const [selectedDemo, setSelectedDemo] = useState(null);
 
-  const selectDemo = (type) => {
-    setSelectedDemo(type);
+  const selectDemo = async (file) => {
+    const Demo = await importDemo(file);
+    const DemoComponent ;
   };
 
   return (
@@ -16,15 +16,18 @@ export default function App() {
         <h1>React Lazy load explained</h1>
 
         <div>
-          <button onClick={() => selectDemo("shape")}>Shape Demo</button>
-          <button onClick={() => selectDemo("color")}>Color Demo</button>
-          <button onClick={() => selectDemo("size")}>Size Demo</button>
+          {demos.map((demo)=>{
+              <button
+              key={demo.name}
+               onClick={() => selectDemo(demo.file)}>
+                {demo.name}
+                </button>
+
+            })}
         </div>
 
         <div>
-          {selectedDemo === "shape" && <ShapeDemo />}
-          {selectedDemo === "color" && <ColorDemo />}
-          {selectedDemo === "size" && <SizeDemo />}
+         {selectedDemo}
         </div>
       </div>
     </div>
