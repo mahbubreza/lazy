a210560fed758f9b658fb20093e9737670e8e007
mahbubreza <mahbubrezachoudhury@gmail.com>
2024-06-11 16:52:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import ColorDemo from "./components/ColorDemo";
+import ShapeDemo from "./components/ShapeDemo";
+import SizeDemo from "./components/SizeDemo";
+
+export default function App() {
+  const [selectedDemo, setSelectedDemo] = useState(null);
+
+  const selectDemo = (type) => {
+    setSelectedDemo(type);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>React Lazy load explained</h1>
+
+        <div>
+          <button onClick={() => selectDemo("shape")}>Shape Demo</button>
+          <button onClick={() => selectDemo("color")}>Color Demo</button>
+          <button onClick={() => selectDemo("size")}>Size Demo</button>
+        </div>
+
+        <div>
+          {selectedDemo === "shape" && <ShapeDemo />}
+          {selectedDemo === "color" && <ColorDemo />}
+          {selectedDemo === "size" && <SizeDemo />}
+        </div>
+      </div>
+    </div>
+  );
+}
